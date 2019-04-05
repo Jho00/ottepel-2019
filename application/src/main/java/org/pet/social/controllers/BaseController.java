e0f082eb0c5473088185b7010c7d81640e14bd6e
Jho00 <akutin_artem@mail.ru>
2019-04-05 20:53:32 +0400

    add resposnes status to webapi

--- application/src/main/java/org/pet/social/controllers/BaseController.java
+++ application/src/main/java/org/pet/social/controllers/BaseController.java
@@ -4,33 +4,41 @@ import org.pet.social.common.responses.ErrorResponse;
 import org.pet.social.common.responses.Response;
 import org.pet.social.common.responses.SuccessResponse;
 
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * Контроллер для наследование, на него реквесты не должны идти
  */
 
 public class BaseController {
 
-    protected Response error()  {
+    protected Response error(HttpServletResponse response)  {
+        response.setStatus(400);
         return new ErrorResponse();
     }
 
-    protected Response error(int code)  {
+    protected Response error(HttpServletResponse response, int code)  {
+        response.setStatus(code);
         return new ErrorResponse(code);
     }
 
-    protected Response error(int code, String message)  {
+    protected Response error(HttpServletResponse response, int code, String message)  {
+        response.setStatus(code);
         return new ErrorResponse(code,message);
     }
 
-    protected Response success(Object object) {
+    protected Response success(HttpServletResponse response, Object object) {
+        response.setStatus(200);
         return new SuccessResponse(object);
     }
 
-    protected Response success(Object object, int code) {
+    protected Response success(HttpServletResponse response, Object object, int code) {
+        response.setStatus(code);
         return new SuccessResponse(object, code);
     }
 
-    protected Response success(Object object, int code, String message) {
+    protected Response success(HttpServletResponse response, Object object, int code, String message) {
+        response.setStatus(code);
         return new SuccessResponse(object, code, message);
     }
 
