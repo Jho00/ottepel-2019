560c2078bd21ed2038fd7eea38a5781a9fd69e5c
Georgiy Kuznetsov <32225372+NesQ1kx@users.noreply.github.com>
2019-04-06 01:10:52 +0400

    Redirect to problem page (#7)

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -1,48 +1,56 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {RouterModule, Routes} from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { MainComponent } from './pages/main/main.component';
-import { AddProblemComponent } from './pages/add-problem/add-problem.component';
-import { ProblemShortComponent } from './components/problem-short/problem-short.component';
+import {HeaderComponent} from './components/header/header.component';
+import {FooterComponent} from './components/footer/footer.component';
+import {MainComponent} from './pages/main/main.component';
+import {AddProblemComponent} from './pages/add-problem/add-problem.component';
+import {ProblemShortComponent} from './components/problem-short/problem-short.component';
 import {HttpClientModule} from "@angular/common/http";
+import {ProblemComponent} from './pages/problem/problem.component';
 
 const appRoutes: Routes = [
-  {
-    path: 'main',
-    component: MainComponent,
-    pathMatch: 'full'
-  },
-  {
-    path: 'add-problem',
-    component: AddProblemComponent,
-    pathMatch: 'full'
-  },
-  {
-    path: '',
-    redirectTo: '/main',
-    pathMatch: 'full'
-  }
+    {
+        path: 'main',
+        component: MainComponent,
+        pathMatch: 'full'
+    },
+    {
+        path: 'add-problem',
+        component: AddProblemComponent,
+        pathMatch: 'full'
+    },
+    {
+        path: '',
+        redirectTo: '/main',
+        pathMatch: 'full'
+    },
+    {
+        path: 'problem/:id',
+        component: ProblemComponent,
+        pathMatch: 'full'
+    }
 ];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    MainComponent,
-    AddProblemComponent,
-    ProblemShortComponent
-  ],
-  imports: [
-    RouterModule.forRoot(appRoutes),
-    BrowserModule,
-      HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        FooterComponent,
+        MainComponent,
+        AddProblemComponent,
+        ProblemShortComponent,
+        ProblemComponent
+    ],
+    imports: [
+        RouterModule.forRoot(appRoutes),
+        BrowserModule,
+        HttpClientModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
