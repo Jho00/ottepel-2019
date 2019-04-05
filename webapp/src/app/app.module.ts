d52a1cfd9aa3d9b6a4a655e993598c7d8efa0449
TryParse <georgiy.kuznecov.98@mail.ru>
2019-04-05 21:18:49 +0400

    Base app

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -2,12 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './pages/main/main.component';
+import { AddProblemComponent } from './pages/add-problem/add-problem.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'main',
+    component: MainComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'add-problem',
+    component: AddProblemComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    redirectTo: '/main',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    AddProblemComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule
   ],
   providers: [],
