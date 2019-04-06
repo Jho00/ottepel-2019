2eb8821a701a1d4e1ac0bcef0f5553792a6b806c
Akutin Artem <akutin_artem@mail.ru>
2019-04-06 04:06:39 +0400

    add simple classifyer (#12)

---
+++ common/src/main/java/org/pet/social/common/enums/GOVERMANT_STRUCTURE.java
@@ -0,0 +1,19 @@
+package org.pet.social.common.enums;
+
+public enum GOVERMANT_STRUCTURE {
+    MVD, // МИНИСТЕРСТВО ВНУТРЕННИХ ДЕЛ
+    GIBDD, // ГИБДД
+    FSB, // ФСБ
+    PROKURATURA, // ПРОКУРАТУРА
+    ROAD_SERVICE, // ДОРОЖНАЯ СЛУЖБА
+    ZHKH, // ЖКХ
+    TRANSPORT_M, // МИНИСТЕРСТВО ТРАНСПОРТА
+    PFR, //ПЕНСИОННЫЙ ФОНД РОССИИ
+    HEALTH_M, // МИНИСТЕРСТВО ЗДРАВООХРАНЕНИЯ
+    CULTURE_M, // МИНИСТЕРСТВО КУЛТУРЫ
+    STM_M, // СПОРТ ТУРИЗМ И МОЛОДЕЖЬ
+    EDUCATION_M, //МИНИСТЕРСТВО ОБРАЗОВАНИЯ
+    MIGRATIONS_SERVICE, // МИГРАЦИОННАЯ СЛУЖБА
+    SANPED // САНАПЕДМСТАНЦИЯ
+
+}
